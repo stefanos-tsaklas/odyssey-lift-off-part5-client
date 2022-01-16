0be42c2d6350be9fd593f39eb0f1697142cb83f0
Stefanos Tsaklas <stsaklas@microsoft.com>
2022-01-16 01:47:14 +0100

    Init

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
+  uri: 'https://server-catstronauts.herokuapp.com/', // change this to YOUR server URI
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
